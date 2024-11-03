7d6ecb5a20d85063d4fc490b00acd2636c7cbe45
Motty S <mottyschneck8@gmail.com>
2024-11-03 20:26:20 +0000

    Maptest

--- src/org/uob/a1/Game.java
+++ src/org/uob/a1/Game.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class Game {
     
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        Room kitchen = new Room("kitchen", "yeah", 'k', new Position(2, 3));
         Map theMap = new Map(5, 5);
-        System.out.println(kitchen.getDescription());
-        theMap.placeRoom(kitchen.getPosition(), kitchen.getSymbol());
+        theMap.placeRoom(new Position(3, 1), 'c');
+        theMap.placeRoom(new Position(0, 0), 'r');
         System.out.println(theMap.display());
     }
     
