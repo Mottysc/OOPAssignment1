84d841134ea06be9c69346553c98ff172386b1fc
Motty S <mottyschneck8@gmail.com>
2024-11-03 20:22:26 +0000

    Score

--- src/org/uob/a1/Score.java
+++ src/org/uob/a1/Score.java
@@ -1,6 +1,24 @@
 package org.uob.a1;
 
 public class Score {
-   
+   private int startingScore;
+   private double currentScore;
+   private int noRoomsVisited;
+   private int noPuzzlesSolves;
+   private final int PUZZLE_VALUE = 10;
+
+   public Score(int startingScore) {
+       this.startingScore = startingScore;
+   }
+   public void visitRoom(){
+       noRoomsVisited++;
+   }
+   public void solvePuzzle(){
+       noPuzzlesSolves++;
+   }
+   public double getScore() {
+       currentScore = startingScore - noRoomsVisited + PUZZLE_VALUE * noPuzzlesSolves;
+       return currentScore;
+   }
         
 }
