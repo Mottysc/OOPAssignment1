191fd473ee5d643d4e503d2797948989354b58b8
Motty S <mottyschneck8@gmail.com>
2024-11-03 19:05:32 +0000

    Inventory

--- src/org/uob/a1/Game.java
+++ src/org/uob/a1/Game.java
@@ -6,6 +6,14 @@ public class Game {
     
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Inventory inventory = new Inventory();
+        inventory.addItem("apple");
+        inventory.addItem("pear");
+        inventory.addItem("banana");
+        System.out.println(inventory.displayInventory());
+        inventory.removeItem("pear");
+        System.out.println(inventory.displayInventory());
+
     }
     
 }
--- src/org/uob/a1/Inventory.java
+++ src/org/uob/a1/Inventory.java
@@ -1,7 +1,43 @@
 package org.uob.a1;
 
 public class Inventory {
-   
+   private String[] items;
+   final int MAX_ITEMS = 10;
+   public Inventory() {
+       items = new String[MAX_ITEMS];
+   }
+
+   public void addItem(String item) {
+       items[items.length - 1] = item;
+   }
+
+   public void removeItem(String item) {
+       String[] temp = new String[items.length];
+       for (int i = 0; i < temp.length; i++) {
+           if (!item.equals(items[i])) {
+               temp[i] = items[i];
+           }
+       }
+       items = temp;
+   }
+
+    public String displayInventory() {
+        String returnString = "";
+        for (int i = 0; i < items.length; i++) {
+            returnString += items[i] + " ";
+        }
+        return returnString;
+    }
+
+    public int hasItem(String item){
+       int itemPosition = -1;
+       for (int i = 0; i < items.length; i++){
+           if (items[i].equals(item)){
+               itemPosition = i;
+           }
+       }
+       return itemPosition;
+   }
 
    
 }
