27793830bb88742201e85150dffd2d0a3e9bc9fb
Motty S <mottyschneck8@gmail.com>
2024-11-03 20:32:14 +0000

    inv test

--- src/org/uob/a1/Game.java
+++ src/org/uob/a1/Game.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Game {
     
     public static void main(String[] args) {
-        Map theMap = new Map(5, 5);
-        theMap.placeRoom(new Position(3, 1), 'c');
-        theMap.placeRoom(new Position(0, 0), 'r');
-        System.out.println(theMap.display());
+        Inventory inv = new Inventory();
+        inv.addItem("sword");
+        System.out.println(inv.displayInventory());
+        System.out.println(inv.hasItem("sword"));
     }
     
 }
